e185cb436badd6990f90514c082628c26184c016
Luan Messias <luanmessias10@gmail.com>
2020-12-28 18:37:48 -0300

    Aplying minicar and header styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,36 @@
 const createHeader = () => {
   // New Html Tgas
   const menuContainerHtml = '<div class="menu_container"></div>';
+  const IconLogin = '<i class="fas fa-sign-in-alt"></i>'
+  const IconCart = '<i class="fas fa-shopping-cart"></i>'
+  const MiniCartCont = '<div class="name-prod"></div>'
 
-  // Tag selectors for already 
+  // Selectors for tags that already exists 
   const mainHeader = $('.menu.superior');
   const mainMenu = $('.menu.superior .nivel-um');
   const logo = $('#cabecalho .logo');
+  const faleConosco = $('.barra-inicial .canais-contato .hidden-phone');
+  const topLinks = $('#cabecalho .acoes-conta');
+  const minhaConta = $('#cabecalho .acoes-conta > li + li');
+  const headerSearchCont = $('#cabecalho .conteudo-topo .inferior');
+  const BemVindo = $('#cabecalho .bem-vindo');
+  const BemVindoSpan = $('#cabecalho .bem-vindo > span');
+  const CarrinhoLinkHeader = $('#cabecalho .inferior .hidden-phone');
+  const MiniCartProd = $('.carrinho-interno > ul > li');
 
   //Actions
-  mainHeader.prepend(menuContainerHtml)
+  mainHeader.prepend(menuContainerHtml);
   $('.menu_container').prepend(logo).append(mainMenu);
+  topLinks.append(faleConosco);
+  CarrinhoLinkHeader.before(BemVindo);
+  BemVindo.prepend(IconLogin);
+  BemVindoSpan.html('LOGAR NA LOJA');
 
+  MiniCartProd.each(function () {
+    $('.preco-produto', this).prepend($('.nome-produto', this));
+  });
 }
+
 
 const setSelectedCategory = () => {
   const bodyClasses = $('body').attr('class').trim().split(' ')
@@ -40,7 +59,7 @@ const TEMP__incrementSubMenu = () => {
 jQuery(document).ready(function () {
   createHeader()
   setSelectedCategory()
-  TEMP__incrementSubMenu()
+  //TEMP__incrementSubMenu()
 });
 
 //Load actions
